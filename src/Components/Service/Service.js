d4ed5d74b7b22f82495f674a2e1b91247fd74bd5
NieamulKabir <kabir000079@gmail.com>
2022-09-09 01:04:57 +0600

    upate

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -9,15 +9,15 @@ const Service = (props) => {
                 <div className="custom-cart-hight md:h-5/6 card lg:card-side bg-base-100 shadow-xl rounded-2x">
                     <figure><img className='h-64 w-full rounded-2xl' src={img} alt="Album" /></figure>
                     <div className="card-body ">
-                        <h2 className="card-title px-5 pt-5 text-xl md:text-2xl lg:text-4xl font-bold  text-violet-700">{title}</h2>
+                        <h2 className="card-title px-5 pt-5 text-lg md:text-2xl lg:text-3xl font-bold  text-violet-700">{title}</h2>
                         <p className="text-sm  text-gray-600">{sector}</p>
-                        <p className="px-5 pt-5 text-lg lg:text-2xl text-red-600">{description}</p>
-                        <p className='text-black font-semibold text-xl'>Fee : {price}</p>
+                        <p className="px-5 pt-5 lg:text-lg md:text-sm text-red-600">{description}</p>
+                        <p className='text-black font-semibold text-lg'>Fee : {price}</p>
 
                         <div className="card-actions">
-                            <button className="btn btn-primary px-2 py-3 mt-6 mr-2 bg-violet-500 custom-bg-font rounded hover:bg-white text-white hover:text-black fond-medium transition duration-300">ENROLL NOW</button>
+                            <button className="btn btn-primary text-xs md:text-sm lg:text-lg px-2 py-3 mt-6 mr-2 bg-violet-500 custom-bg-font rounded hover:bg-white text-white hover:text-black fond-medium transition duration-300">ENROLL NOW</button>
 
-                            <button className="btn btn-primary px-5 py-3 mt-6  ml-2 bg-violet-500 custom-bg-font rounded hover:bg-white text-white hover:text-black fond-medium transition duration-300">DETAILS</button>
+                            <button className="btn btn-primary text-xs md:text-sm lg:text-lg px-5 py-3 mt-6  ml-2 bg-violet-500 custom-bg-font rounded hover:bg-white text-white hover:text-black fond-medium transition duration-300">DETAILS</button>
 
                         </div>
                     </div>
